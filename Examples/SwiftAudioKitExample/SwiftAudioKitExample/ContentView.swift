b333b18f55c0044a5f65010e12520708c29bb52f
Dionysis Karatzas <diokaratzas@gmail.com>
2024-08-04 15:07:24 +0300

    Initial Commit

---
+++ Examples/SwiftAudioKitExample/SwiftAudioKitExample/ContentView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import SwiftAudioKit
+import Combine
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
---
+++ Examples/SwiftAudioKitExample/SwiftAudioKitExample/SwiftAudioKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAudioKitExampleApp.swift
+//  SwiftAudioKitExample
+//
+//  Created by Dionisis Karatzas on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAudioKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
