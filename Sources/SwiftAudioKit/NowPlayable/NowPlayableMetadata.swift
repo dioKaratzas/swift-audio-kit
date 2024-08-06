dca7d1c950d7576ba26d17b7b5cf25819b054ee0
Dionysis Karatzas <diokaratzas@gmail.com>
2024-08-06 23:17:18 +0300

    Add NowPlayableMetadata public constructors

--- Sources/SwiftAudioKit/NowPlayable/NowPlayableMetadata.swift
+++ Sources/SwiftAudioKit/NowPlayable/NowPlayableMetadata.swift
@@ -18,6 +18,18 @@ public struct NowPlayableStaticMetadata {
     public let album: String? // MPMediaItemPropertyAlbumTitle
     public let trackCount: NSNumber? // MPMediaItemPropertyAlbumTrackCount
     public let trackNumber: NSNumber? // MPMediaItemPropertyAlbumTrackNumber
+
+    public init(assetURL: URL, mediaType: MPNowPlayingInfoMediaType, isLiveStream: Bool, title: String?, artist: String?, artwork: MPMediaItemArtwork?, album: String?, trackCount: NSNumber?, trackNumber: NSNumber?) {
+        self.assetURL = assetURL
+        self.mediaType = mediaType
+        self.isLiveStream = isLiveStream
+        self.title = title
+        self.artist = artist
+        self.artwork = artwork
+        self.album = album
+        self.trackCount = trackCount
+        self.trackNumber = trackNumber
+    }
 }
 
 public struct NowPlayableDynamicMetadata {
@@ -26,4 +38,12 @@ public struct NowPlayableDynamicMetadata {
     public let duration: Float // MPMediaItemPropertyPlaybackDuration
     public let currentLanguageOptions: [MPNowPlayingInfoLanguageOption] // MPNowPlayingInfoPropertyCurrentLanguageOptions
     public let availableLanguageOptionGroups: [MPNowPlayingInfoLanguageOptionGroup] // MPNowPlayingInfoPropertyAvailableLanguageOptions
+
+    public init(rate: Float, position: Float, duration: Float, currentLanguageOptions: [MPNowPlayingInfoLanguageOption], availableLanguageOptionGroups: [MPNowPlayingInfoLanguageOptionGroup]) {
+        self.rate = rate
+        self.position = position
+        self.duration = duration
+        self.currentLanguageOptions = currentLanguageOptions
+        self.availableLanguageOptionGroups = availableLanguageOptionGroups
+    }
 }
